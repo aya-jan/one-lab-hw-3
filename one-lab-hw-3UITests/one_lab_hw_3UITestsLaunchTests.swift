78e9867671a5f0d75e92d0c08cc373093eea666a
aya <190107024@stu.sdu.edu.kz>
2022-04-22 02:04:21 +0600

    Initial Commit

---
+++ one-lab-hw-3UITests/one_lab_hw_3UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  one_lab_hw_3UITestsLaunchTests.swift
+//  one-lab-hw-3UITests
+//
+//  Created by aya on 22.04.2022.
+//
+
+import XCTest
+
+class one_lab_hw_3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
